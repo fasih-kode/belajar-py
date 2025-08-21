544be33780bf0e6a6f79e9bf6d8ee5bd63700f3b
fasih-kode <fasih@tutanota.com>
2025-08-21 20:41:17 +0700

    Tambah file app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+print('Hallo Bos!')
+print('Siapa anda?!')
+print('O----')
+print(' ||||')
+print('*' * 10)
+print('>', '-' * 10, '>')
+
+# Variabel
+
+harga = 1000       # int
+rating = 4.5       # float
+nama = "Fasih"     # str
+# is_published = True  # bool
+
+name = 'Jhon Smith'
+old = 20
+is_new = True
+
+harga = 1500
+rating = 4.7
+nama = "Buku Tulis"
+is_published = True
+print(harga, rating, nama, is_published)
+print('Produk', nama)
+print('Harga', harga)
+print('Rating', rating)
+print('Apakah sudah dipublikasikan?', is_published)
+
+# input dari user
+nama = input('Masukkan nama produk:')
+harga = input('Masukkan harga produk:')
+print('Produk', nama, 'harganya', harga)
